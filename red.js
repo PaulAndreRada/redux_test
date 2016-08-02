323976ecc07edcec192c7dcf90dcbe44b69e58df
Paul Andre Rada <paulriverarada@gmail.com>
2016-08-01 17:26:33 -0700

    first commit

---
+++ red.js
@@ -0,0 +1,53 @@
+import {combineReducers, createStore } from 'redux'
+
+// actions
+const ADD_TODO = 'ADD_TODO'
+const INCREMENT_KEY = 'INCREMENT_KEY'
+
+// action creator
+function addTodo(text){
+  return {
+    type: ADD_TODO,
+    text
+  }
+}
+
+// reducer
+function todos( state = [], action){
+  switch(action.type){
+    case ADD_TODO:
+     return [
+       ...state,
+       {
+         text: action.text,
+         completed: false,
+       }
+     ]
+     default:
+      return state
+  }
+};
+
+const todoApp = combineReducers({
+  todos
+})
+
+// store
+let store = createStore(todoApp);
+
+// log the initial state
+console.log( store.getState())
+
+// Every time the state changes, log it
+// returns an unsubscribe object
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+
+// dispatch some actions
+store.dispatch(addTodo('make your first action'))
+store.dispatch(addTodo('second action here'))
+
+
+// Stop listening to state updates
+unsubscribe()
